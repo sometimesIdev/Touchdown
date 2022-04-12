166a6e7f745dc55f3c16547aaea566cd0ccc5bc6
Pat Butler <15131675+Beefycoder@users.noreply.github.com>
2022-04-12 15:06:38 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Pat Butler on 2022-04-12.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
